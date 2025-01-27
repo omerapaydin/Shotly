529b91dddffc51562fa1daf7c6258b7f345df077
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-28 01:21:38 +0300

    Düzeltme

--- obj/Debug/net9.0/Shotly.AssemblyInfo.cs
+++ obj/Debug/net9.0/Shotly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shotly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b177a550673622a06cdaa40fb8d5c7610e6f62f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97e82f622cb73186ce799375069625aea6bebbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shotly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shotly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
